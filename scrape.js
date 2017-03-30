9b468bc7026cc54eedfd7ddf665e087bf247d9c0
Michael Theriot <michael.lee.theriot@gmail.com>
2017-03-29 21:24:50 -0500

    initial commit

---
+++ scrape.js
@@ -0,0 +1,56 @@
+if (process.argv.length < 6) {
+  console.log('usage: node scrape username password tenant_id filename [next_link]');
+  return;
+}
+
+const
+  msRestAzure = require('ms-rest-azure'),
+  graphRbacManagementClient = require('azure-graph'),
+  fs = require('fs'),
+  username = process.argv[2],
+  password = process.argv[3],
+  tenantId = process.argv[4],
+  filename = process.argv[5],
+  initLink = process.argv[6] || null;
+
+const login = (username, password) =>
+  new Promise((resolve, reject) =>
+    msRestAzure.loginWithUsernamePassword(username, password, {tokenAudience: 'graph', domain: tenantId}, (err, credentials, subscriptions) => err ? reject(err) : resolve({credentials, subscriptions})));
+
+const queryUsers = (client, nextLink = null) =>
+  new Promise((resolve, reject) =>
+    nextLink
+      ? client.users.listNext(nextLink, (err, result, request, response) =>
+          err ? reject(err) : resolve({result: [...result], nextLink: result.odatanextLink}))
+      : client.users.list((err, result, request, response) =>
+          err ? reject(err) : resolve({result: [...result], nextLink: result.odatanextLink})));
+
+async function scrape() {
+  const
+    client = new graphRbacManagementClient((await login(username, password)).credentials, tenantId),
+    out = fs.createWriteStream('./' + filename, {flags: 'w'});
+  let
+    result,
+    nextLink = null;
+  while(({result, nextLink} = await queryUsers(client, nextLink)) && result.length == 100) {
+    result.forEach(item => {
+      const {objectId, objectType, userPrincipalName, displayName, mail, mailNickname} = item;
+      out.write(objectId + '\t' + objectType + '\t' + userPrincipalName + '\t' + displayName + '\t' + mail + '\t' + mailNickname + '\n');
+    });
+    console.log(nextLink);
+  }
+  out.end();
+}
+
+async function main() {
+  console.log('start: ' + new Date());
+  try {
+    await scrape();
+  } catch (err) {
+    console.error(err);
+    console.log('pass the last link as a parameter to restart from this point');
+  }
+  console.log('end: ' + new Date());
+};
+
+main();
